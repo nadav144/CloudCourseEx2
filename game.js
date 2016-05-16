b61a44a32177e4885015c180e57f27e2a2c4ae36
odedex <karitvruda@hotmail.com>
2016-05-16 15:51:18 +0300

    games are now stored in a separate module

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
 
-module.exports.game = function (roomid, player) {
+module.exports.game = function (roomid, player, gamelines) {
     this.id = roomid;
     this.messages = [];
     this.curTurn = player;
+    this.maxLines = gamelines;
+    this.gameLines = 0;
 
     this.addMsg = function (player, msg) {
         this.messages.push({player: player, msg: msg});
+        this.gameLines += 1;
     };
 
     return this;
